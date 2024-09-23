5d768439efe1fa731390014bb4e239c5206f5e46
Adam <adam.tamas@ni.com>
2024-09-23 11:39:45 +0200

    Add fetch

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,12 +1,33 @@
 import Link from "next/link";
 
-export default function About() {
+interface Product {
+  id: number;
+  title: string;
+}
+
+interface ProductsResponse {
+  products: Product[];
+}
+
+/* export const runtime = "edge"; */
+
+export default async function About() {
+  const res = await fetch("https://dummyjson.com/products");
+  const data: ProductsResponse = await res.json();
+  const products: Product[] = data.products;
+
   return (
     <>
-      <h1>About</h1>
+      <h1>This page will use the edge runtime to fetch data.</h1>
       <div>
         Back to <Link href="/">Home</Link>
       </div>
+      <h3>Products</h3>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.title}</li>
+        ))}
+      </ul>
     </>
   );
 }
